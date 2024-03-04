f2a833396d6a4ffa1f4285c07385d707aaf433d5
Kayode Oluwasegun <segunkayode00@gmail.com>
2024-03-04 06:18:20 +0100

    config: setup catpuccin theme

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
